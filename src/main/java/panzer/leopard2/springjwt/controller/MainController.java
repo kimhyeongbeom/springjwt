5d567dad5471907f9e95480a1943864b79538265
hyeongbeom-kim <leopard2@wizvil.com>
2025-02-09 11:02:41 +0900

    1st commit

---
+++ src/main/java/panzer/leopard2/springjwt/controller/MainController.java
@@ -0,0 +1,15 @@
+package panzer.leopard2.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/")
+    public String getRoot() {
+        return "Main Controller";
+    }
+}
